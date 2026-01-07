59e2836ec6d3b6e73d75a9a1297fbe4e11de53a9
Reinier Martin <contact@reiniermartin.com>
2026-01-07 18:30:41 +0100

    Add pong_v040.js file
    Changed trigger time to 4.5 secs

--- pong_v040.js
+++ pong_v040.js
@@ -357,7 +357,7 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   // Long press detector for spacebar, mouse clicks, and touch
   class LongPressDetector {
-    constructor(holdTime = 9500) {
+    constructor(holdTime = 4500) {
       this.holdTime = holdTime;
       this.pressStart = 0;
       this.pressTimer = null;
